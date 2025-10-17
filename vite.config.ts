fc0c88bed55dbdbe2153ad839f16ee762f940ca1
ztoy757 <52619924+ztoy757@users.noreply.github.com>
2025-10-17 17:59:58 +0000

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/sdd_cursor/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ztoy757.github.io/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
